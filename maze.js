50727bc23a42b7b28c9bfe2990e5bc227be6c3a8
thinkking <lee.christopher.murray@gmail.com>
2014-10-04 04:50:09 +0000

    first upload

---
+++ maze.js
@@ -0,0 +1,34 @@
+var loser = false;  // whether the user has hit a wall
+
+window.onload = function() {
+    $("start").onclick = startClick;
+    $("end").onmouseover = overEnd;
+    var boundaries = $$("div#maze div.boundary");
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].onmouseover = overBoundary;
+    }
+};
+
+function overBoundary() {
+    loser = true;
+    $("status").textContent = "You lose!";
+    var boundaries = $$("div#maze div.boundary");
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].addClassName("youlose");
+    }
+}
+
+function startClick() {
+    loser = false;
+    $("status").textContent = "Find the end!";
+    var boundaries = $$("div#maze div.boundary");
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].removeClassName("youlose");
+    }
+}
+
+function overEnd() {
+    if (!loser) {
+        $("status").textContent = "You win! :]";
+    }
+}
